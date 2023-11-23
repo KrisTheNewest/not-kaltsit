4a8b0f7d741de211f8d9ea8004f7815feb15ce65
KrisTheNewest <61835777+KrisTheNewest@users.noreply.github.com>
2023-11-23 17:48:35 +0100

    basic twitter news poster

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require("discord.js");
 const { readdir } = require('fs/promises');
 const register = require("./register.js");
+const twitter = require("./posters/twitterNews.js")
 
 // TODO: POSSIBLY TURN THE COMMAND FILE INTO AN ARRAY
 // TODO: OR DO SOMETHING WITH THE NAMING SCHEME
@@ -10,21 +11,25 @@ const client = new Client({ intents: [ GatewayIntentBits.Guilds ] });
 
 const commandMap = new Map();
 
-client.once("ready", async () => {
+client.once("ready", async (c) => {
 	// catching errors in the ready block is pointless
 	// if anything here fails the bot won't function properly
-
 	const commandData = (await readdir("./commands", "utf-8"))
 		.filter(c => c.endsWith(".js"))
 		.map(c => require("./commands/" + c));
 
-	// console.log(commandData);
 	const toBeRegistered = commandData.map(c => c.commandInfo);
 	await register(toBeRegistered);
+
 	commandData.forEach(({ commandInfo, command }) => {
 		commandMap.set(commandInfo.name, command);
 	});
-	console.log(commandMap);
+
+	const newsChannel = c.channels.cache.get(process.env.DEBUG_NEWS_CHANNEL);
+	setInterval(() => { 
+		twitter(newsChannel);
+	}, 60 * 1000);
+	
 	console.log("Connected and ready!");
 });
 
